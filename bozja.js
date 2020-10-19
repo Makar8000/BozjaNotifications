c752ec7eafa1e18c508ff05ecb0fc74049dc98e7
Makar8000 <Makar8000@users.noreply.github.com>
2020-10-18 23:46:50 -0500

    first commit

---
+++ bozja.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const shouldFire = (fate, option) => {
+  if (!fateConfig.options.types.find(type => type === option))
+    return false;
+  if (option === 'crit' && !fate.spawnsCrit)
+    return false;
+  if (option === 'solo' && !fate.spawnsSolo)
+    return false;
+
+  return true;
+}
+
+const sendTTSNotification = fate => {
+  if (!shouldFire(fate, fateConfig.options.tts))
+    return;
+
+  let cmd = { 'call': 'cactbotSay', 'text': `${fate.name}` };
+  window.callOverlayHandler(cmd);
+}
+
+const sendPushNotification = fate => {
+  if (!shouldFire(fate, fateConfig.options.push))
+    return;
+
+  const pushOptions = {
+    token: fateConfig.options.pushConfig.token,
+    user: fateConfig.options.pushConfig.user,
+    title: "Fate Pop",
+    message: fate.name
+  }
+
+  if (fateConfig.options.pushConfig.device)
+    pushOptions.device = fateConfig.options.pushConfig.device;
+
+  fetch('https://api.pushover.net/1/messages.json', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(pushOptions)
+  }).then(response => {
+    if (!response.ok)
+      console.error("Unable to send notification for fate", fate);
+  });
+}
+
+const onFate = e => {
+  if (e.detail.eventType !== 'add')
+    return;
+
+  const fate = fateConfig.fates.find(fate => fate.id === e.detail.fateID);
+  if (!fate)
+    return;
+
+  sendTTSNotification(fate)
+  sendPushNotification(fate);
+}
+
+window.addEventListener('DOMContentLoaded', async (e) => {
+  window.addOverlayListener('onFateEvent', e => onFate(e));
+  window.startOverlayEvents();
+});
---
+++ fate-config.js
@@ -0,0 +1,209 @@
+// Bozja
+// Southern Front: https://xivapi.com/search?indexes=Fate&filters=ID%3E=1597,ID%3C=1628&columns=Description,Name,Url
+
+const fateConfig = {
+  options: {
+    tts: 'solo',
+    push: '',
+    types: ['all', 'crit', 'solo'],
+    pushConfig: {
+      token: '<token>',
+      user: '<user>',
+      device: undefined,
+    },
+  },
+  fates: [
+    {
+      id: 1597,
+      name: 'Sneak & Spell',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1598,
+      name: 'None of Them Knew They Were Robots',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1599,
+      name: 'The Beasts Must Die',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1600,
+      name: 'Unrest for the Wicked',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1601,
+      name: 'More Machine Now than Man',
+      spawnsCrit: true,
+      spawnsSolo: true
+    },
+    {
+      id: 1602,
+      name: 'Can Carnivorous Plants Bloom Even on a Battlefield?',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1603,
+      name: 'Seeq and Destroy',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1604,
+      name: 'All Pets Are Off',
+      spawnsCrit: true,
+      spawnsSolo: false
+    },
+    {
+      id: 1605,
+      name: 'Conflicting with the First Law',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1606,
+      name: 'Brought to Heal',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1607,
+      name: 'The Monster Mash',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1608,
+      name: 'Red (Chocobo) Alert',
+      spawnsCrit: true,
+      spawnsSolo: true
+    },
+    {
+      id: 1609,
+      name: 'Unicorn Flakes',
+      spawnsCrit: true,
+      spawnsSolo: false
+    },
+    {
+      id: 1610,
+      name: 'Parts and Recreation',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1611,
+      name: 'The Element of Supplies',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1612,
+      name: 'Heavy Boots of Lead',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1613,
+      name: 'No Camping Allowed',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1614,
+      name: 'Scavengers of Human Sorrow',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1615,
+      name: 'Help Wanted',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1616,
+      name: 'Pyromancer Supreme',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1617,
+      name: 'Waste the Rainbow',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1618,
+      name: 'The Wild Bunch',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1619,
+      name: 'My Family and Other Animals',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1620,
+      name: 'I\'m a Mechanical Man',
+      spawnsCrit: true,
+      spawnsSolo: false
+    },
+    {
+      id: 1621,
+      name: 'Murder Death Kill',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1622,
+      name: 'Desperately Seeking Something',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1623,
+      name: 'Supplies Party',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1624,
+      name: 'Demonstrably Demonic',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1625,
+      name: 'For Absent Friends',
+      spawnsCrit: true,
+      spawnsSolo: true
+    },
+    {
+      id: 1626,
+      name: 'Of Steel and Flame',
+      spawnsCrit: true,
+      spawnsSolo: true
+    },
+    {
+      id: 1627,
+      name: 'Let Slip the Dogs of War',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1628,
+      name: 'The War Against the Machines',
+      spawnsCrit: false,
+      spawnsSolo: false
+    }
+  ]
+}
