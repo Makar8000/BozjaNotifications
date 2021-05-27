bdb176f60ad97470b1113513874fcc7411aca9c0
Makar8000 <Makar8000@users.noreply.github.com>
2021-05-26 20:56:32 -0500

    add zadnor fates

--- fate-config.js
+++ fate-config.js
@@ -204,6 +204,162 @@ const fateConfig = {
       name: 'The War Against the Machines',
       spawnsCrit: false,
       spawnsSolo: false
-    }
+    },
+    {
+      id: 1717,
+      name: 'Of Beasts and Braggadocio',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1718,
+      name: 'Parts and Parcel',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1719,
+      name: 'An Immoral Dilemma',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1720,
+      name: 'Deadly Divination',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1721,
+      name: 'A Wrench in the Reconnaissance Effort',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1722,
+      name: 'Another Pilot Episode',
+      spawnsCrit: true,
+      spawnsSolo: true
+    },
+    {
+      id: 1723,
+      name: 'Breaking the Ice',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1724,
+      name: 'Meet the Puppetmaster',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1725,
+      name: 'Challenge Accepted',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1726,
+      name: 'Th\'uban the Terrible',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1727,
+      name: 'An End to Atrocities',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1728,
+      name: 'A Just Pursuit',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1729,
+      name: 'Tanking Up',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1730,
+      name: 'Supersoldier Rising',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1731,
+      name: 'Demented Mentor',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1732,
+      name: 'Sever the Strings',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1733,
+      name: 'The Beasts Are Back',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1734,
+      name: 'Still Only Counts as One',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1735,
+      name: 'Seeq and You Will Find',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1736,
+      name: 'Mean-spirited',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1737,
+      name: 'A Relic Unleashed',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1738,
+      name: 'When Mages Rage',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1739,
+      name: 'Hypertuned Havoc',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1740,
+      name: 'Attack of the Supersoldiers',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1741,
+      name: 'The Student Becalms the Master',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
+    {
+      id: 1742,
+      name: 'Attack of the Machines',
+      spawnsCrit: false,
+      spawnsSolo: false
+    },
   ]
 }
